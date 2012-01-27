3291ddfb8677ccfdc1c339ddac8991515039cd91
Steve McIntyre <steve@stevedecoded.com>
2012-01-27 23:03:16 +0000

    changes to Entity class and tmx filepath

--- src/entity.lua
+++ src/entity.lua
@@ -2,7 +2,25 @@ require('object')
 
 Entity = Object:new()
 
+function Entity:init()
+	graphic = nil
+	x = 0
+	y = 0
+end
+
+function Entity:setGraphic(filepath)
+	self.graphic = love.graphics.newImage(filepath)
+end
+
+function Entity:setPosition(x, y)
+	self.x = x
+	self.y = y
+end
+
 function Entity:draw()
+	if self.graphic ~= nil then
+		love.graphics.draw(self.graphic, self.x, self.y)
+	end
 end
 
 function Entity:update(dtime)
