b7a3f34139f84cfdf9aec72fff172f7ee5e179c0
CakeCatastrophe <atolm1@hotmail.com>
2012-01-28 09:53:34 +0000

    Conveyer object had no code, added basic entity stuff.

--- src/conveyor.lua
+++ src/conveyor.lua
@@ -1,1 +1,12 @@
+require('entity')
+require('sprite')
 
+Conveyor = Entity:new()
+
+function Conveyor:init()
+	self.graphic = Sprite:new('graphics/conveyor.png', 32, 32)
+end
+
+function Conveyor:update(dtime)
+	self.graphic:update(dtime)
+end
