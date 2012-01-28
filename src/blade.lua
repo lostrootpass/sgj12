f0b0f440109e5ba33efb077b820cfe5b5e02df58
Alex McGivern <alex@aeonofdiscord.com>
2012-01-28 02:04:56 +0000

    Spinning blade.

---
+++ src/blade.lua
@@ -0,0 +1,17 @@
+require('entity')
+require('sprite')
+
+Blade = Entity:new()
+
+function Blade:init()
+	self.sprite = Sprite:new('assets/graphics/blade.png', 32, 32)
+	self.sprite:add("spin", {1, 2, 3}, 0.05)
+end
+
+function Blade:draw()
+	self.sprite:draw()
+end
+
+function Blade:update(dtime)
+	self.sprite:update(dtime)
+end
