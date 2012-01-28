71671c448d0790ea2bd221aa1828a3d1beab6d85
Alex McGivern <alex@aeonofdiscord.com>
2012-01-28 21:39:24 +0000

    Fixed player positioning on transition.

--- src/universe.lua
+++ src/universe.lua
@@ -17,13 +17,36 @@ function Universe:loadArea(file)
 	return world
 end
 
-function Universe:moveToArea(areaName)
+function Universe:moveToArea(areaName, direction)
 	local area = self:loadArea(areaName)
 	State.world = area
 	State.player = Player:new()
 	area:add(State.player)
-	State.player.x = 96
-	State.player.y = 96
+	
+	local opposites = {n = 's', s = 'n', w = 'e', e = 'w'}
+	
+	door = nil
+	for _, k in ipairs(State.world.entities) do
+		if k.dir == opposites[direction] then
+			door = k
+		end
+	end
+	
+	if door then
+		if direction == 's' then
+			State.player.x = door.x + 16
+			State.player.y = door.y + 32
+		elseif direction == 'n' then
+			State.player.x = door.x + 16
+			State.player.y = door.y - 32
+		elseif direction == 'w' then
+			State.player.x = door.x - 32
+			State.player.y = door.y + 16
+		elseif direction == 'e' then
+			State.player.x = door.x + 32
+			State.player.y = door.y + 16
+		end
+	end
 	
 	State.player.graphic:play('stand_right')
 end
