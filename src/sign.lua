ca447ec19c3d70ada8c3d4fa40dca63c7d14c5b0
Alex McGivern <alex@aeonofdiscord.com>
2012-02-02 22:49:31 +0000

    Signs are read automatically.

--- src/sign.lua
+++ src/sign.lua
@@ -9,6 +9,7 @@ function Sign:init()
 	self.sprite = Image:new('graphics/sign.png')
 	self.text = "This is a sign."
 	self.hitbox = Hitbox:new(0, 32, 32, 32)
+	self.showing = false
 end
 
 function Sign:draw()
@@ -20,10 +21,14 @@ function Sign:update(dtime)
 	self.sprite.y = self.y
 	self.hitbox.x = self.x
 	self.hitbox.y = self.y + 32
-	
-	if love.keyboard.isDown(' ') then
-		if self.hitbox:intersects(State.player.hitbox) then
+
+	if self.hitbox:intersects(State.player.hitbox) then
+		if not self.showing then
 			Dialogue:show(self.text)
+			self.showing = true
 		end
+	elseif self.showing then
+		Dialogue:hide()
+		self.showing = false
 	end
 end
